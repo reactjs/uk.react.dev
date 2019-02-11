dfd26bd6cae978182b51c1cc05721b846d171519
Michael Babin <iamsuperman@ukr.net>
2019-02-11 17:26:03 +0200

    Added translation for rendering elements and code snippets

--- examples/rendering-elements/update-rendered-element.js
+++ examples/rendering-elements/update-rendered-element.js
@@ -1,7 +1,11 @@
 function tick() {
   const element = (
     <div>
+<<<<<<< HEAD
       <h1>Hello, world!</h1>
+=======
+      <h1>Привіт, світе</h1>
+>>>>>>> ef4344de... Added translation for rendering elements and code snippets
       <h2>Зараз {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
