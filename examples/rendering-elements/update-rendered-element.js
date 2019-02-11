cec9a10577a46cd5f3eda017b72add4a55c864fc
Michael Babin <iamsuperman@ukr.net>
2019-02-11 22:11:41 +0200

    fix requested changes

--- examples/rendering-elements/update-rendered-element.js
+++ examples/rendering-elements/update-rendered-element.js
@@ -1,7 +1,7 @@
 function tick() {
   const element = (
     <div>
-      <h1>Привіт, світе</h1>
+      <h1>Hello, world!</h1>
       <h2>Зараз {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
