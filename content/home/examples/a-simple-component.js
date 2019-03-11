7adab09359ac78ecfee47639caaaa1e2843561ed
Kurzdor <pavloeksmidt@gmail.com>
2019-03-11 04:10:39 +0200

    chore: translated some components on home page

--- content/home/examples/a-simple-component.js
+++ content/home/examples/a-simple-component.js
@@ -1,14 +1,10 @@
 class HelloMessage extends React.Component {
   render() {
-    return (
-      <div>
-        Hello {this.props.name}
-      </div>
-    );
+    return <div>Привіт, {this.props.name}</div>;
   }
 }
 
 ReactDOM.render(
-  <HelloMessage name="Taylor" />,
-  document.getElementById('hello-example')
+  <HelloMessage name="Петро" />,
+  document.getElementById('hello-example'),
 );
