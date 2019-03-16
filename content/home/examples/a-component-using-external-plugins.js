486056b9d3838ade1dc01dc2b758a409ff04b4a7
Paul Ekshmidt <pavloeksmidt@gmail.com>
2019-03-16 18:22:43 +0200

    Translation of some components on home page (#64)
    * chore: translated some components on home page
    
    * fix: fix codestyle

--- content/home/examples/a-component-using-external-plugins.js
+++ content/home/examples/a-component-using-external-plugins.js
@@ -2,7 +2,7 @@ class MarkdownEditor extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = { value: 'Hello, **world**!' };
+    this.state = { value: 'Привіт, **світе**!' };
   }
 
   handleChange(e) {
@@ -17,16 +17,16 @@ class MarkdownEditor extends React.Component {
   render() {
     return (
       <div className="MarkdownEditor">
-        <h3>Input</h3>
+        <h3>Редактор</h3>
         <label htmlFor="markdown-content">
-          Enter some markdown
+          Введіть що-небудь в форматі Markdown
         </label>
         <textarea
           id="markdown-content"
           onChange={this.handleChange}
           defaultValue={this.state.value}
         />
-        <h3>Output</h3>
+        <h3>Вивід</h3>
         <div
           className="content"
           dangerouslySetInnerHTML={this.getRawMarkup()}
