9a3a684d4837cc426fadc44d3a8d17b835d84156
Rostyslav Pasichnyk <22272939+pasichnyk@users.noreply.github.com>
2019-08-28 19:27:46 +0300

    Apply suggestions from code review
    Co-Authored-By: Oleksii Polovyi <oleksii.polovyi@gmail.com>

--- examples/forwarding-refs/fancy-button-ref.js
+++ examples/forwarding-refs/fancy-button-ref.js
@@ -3,7 +3,7 @@ import FancyButton from './FancyButton';
 // highlight-next-line
 const ref = React.createRef();
 
-// Компонент FancyButton, який ми імпортуємо - це КВП LogProps.
+// Компонент FancyButton, який ми імпортуємо — це КВП LogProps.
 // Навіть, якщо результат рендерингу буде таким же самим,
 // Наш реф буде вказувати на LogProps, а не на внутрішній компонент FancyButton!
 // Це означає, що ми, наприклад, не можемо викликати ref.current.focus()
